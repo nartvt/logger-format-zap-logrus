ee764907686c3374605158b1887fde21b0c83677
taitran <taitv@nct.vn>
2021-07-16 17:39:23 +0700

    add log format as spring

---
+++ go.mod
@@ -0,0 +1,12 @@
+module logrus-format
+
+go 1.16
+
+require (
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816 // indirect
+	github.com/t-tomalak/logrus-prefixed-formatter v0.5.2 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+)
---
+++ program.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	easy "github.com/t-tomalak/logrus-easy-formatter"
+	prefixed "github.com/t-tomalak/logrus-prefixed-formatter"
+	"runtime"
+	"sort"
+	"strings"
+)
+
+func initLogFormat() {
+	log.SetReportCaller(true)
+	//logger := log.New()
+	//buffer := &bytes.Buffer{}
+	//logger.Out = buffer
+	//logger.SetReportCaller(true)
+	textFormat := &log.TextFormatter{
+		//DisableColors: true,
+		TimestampFormat:  "2006-01-02 15:04:05",
+		CallerPrettyfier: runTime,
+		FullTimestamp:    true,
+		SortingFunc: func(s []string) {
+			sort.Strings(s)
+		},
+		DisableSorting: false,
+		ForceColors:    false,
+		ForceQuote:     false,
+	}
+	//entry := &log.Entry{
+	//	Logger: logger,
+	//}
+	//textFormat.Format(entry)
+	log.SetFormatter(textFormat)
+}
+func initEasy() {
+	//pc, fn, line, _ := runtime.Caller(1)
+	formatter := &easy.Formatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		LogFormat:       "%time% [%lvl%] - %msg% ",
+	}
+	log.SetFormatter(formatter)
+}
+func initPrefixFormat() {
+	formatter := &prefixed.TextFormatter{
+		DisableColors:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+		ForceFormatting: true,
+
+	}
+	log.SetFormatter(formatter)
+}
+func fileLine(toFile string) string {
+	arr := strings.Split(toFile, "/")
+	return arr[len(arr)-1]
+}
+
+func runTime(f *runtime.Frame) (string, string) {
+	return "", fmt.Sprintf("\t%s - line %d", fileLine(f.File), f.Line)
+}
+
+func init(){
+	//initLogFormat()
+	initEasy()
+	//initPrefixFormat()
+}
+func main() {
+	log.Info("After show format time 738")
+	fmt.Println("")
+	format("Message test line and func")
+}
+
+func format(msg string) {
+	pc, fn, line, _ := runtime.Caller(1)
+	log.Fatalf("[%s] [%s:%d] %v", runtime.FuncForPC(pc).Name(), fn, line,msg)
+}
