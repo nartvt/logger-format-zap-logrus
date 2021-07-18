a5777c86a6e342fa5dbe1c7bd038c0113f3e1b90
taitran <tooyoutooyou@gmail.com>
2021-07-18 23:34:40 +0700

    add zap sugger log format

--- logger/uber_zap.go
+++ logger/uber_zap.go
@@ -57,3 +57,25 @@ func SyslogTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 func CustomLevelEncoder(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString("[" + level.CapitalString() + "]")
 }
+// sugar zap
+func ConfigZap() *zap.SugaredLogger{
+
+	cfg := zap.Config{
+		Encoding:    "json", //encode kiểu json hoặc console
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),   //chọn InfoLevel có thể log ở cả 3 level
+		OutputPaths: []string{"stderr"},
+
+		EncoderConfig: zapcore.EncoderConfig{   //Cấu hình logging, sẽ không có stacktracekey
+			MessageKey: "message",
+			TimeKey: "time",
+			LevelKey: "level",
+			CallerKey:    "caller",
+			EncodeCaller: zapcore.FullCallerEncoder,    //Lấy dòng code bắt đầu log
+			EncodeLevel: CustomLevelEncoder,    //Format cách hiển thị level log
+			EncodeTime: SyslogTimeEncoder,  //Format hiển thị thời điểm log
+		},
+	}
+
+	logger, _ := cfg.Build()    //Build ra Logger
+	return logger.Sugar()   //Trả về logger hoặc Sugaredlogger, ở đây ta chọn trả về Logger
+}
